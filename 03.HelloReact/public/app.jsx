a2020f858d7c0a8d191dd3e8ce08eda93dca845a
arturisav <artur.isaverdyan@gmail.com>
2018-01-04 16:06:05 +0400

    user events

--- 03.HelloReact/public/app.jsx
+++ 03.HelloReact/public/app.jsx
@@ -1,14 +1,21 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
 
 var Greeter = React.createClass({
     getDefaultProps: function () {
         return {
-            name: 'React'
+            name: 'React',
+            message: 'This is the default message!'
         };
+    },
+    onButtonClick: function (e) {
+        e.preventDefault();
+
+        var prm = this.refs.prm.value;
+        alert(prm);
+
     },
     render: function () {
         var name = this.props.name;
+        var message = this.props.message;
 /*        return React.createElement(
             'h1',
             null,
@@ -17,17 +24,21 @@ var Greeter = React.createClass({
         return (
             <div>
                 <h1>Hello {name} !!!</h1>
-                <p>This is from component too</p>
+                <p>{message + '!!'}</p>
+
+                <form onSubmit={this.onButtonClick}>
+                    <input type="text" ref="prm"/>
+                    <button >Click Me</button>
+                </form>
             </div>
         );
     }
 });
 
-var firstname = "Artur"
+var firstName = 'Artur';
 
 ReactDOM.render(
-    <Greeter name={firstname}/>,
+    <Greeter name={firstName}/>,
     //<Greeter />,        //with default atribute name, its value is React, on line 7:
-
     document.getElementById("app")
-)
+);
