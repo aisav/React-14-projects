48b8a2a893c3c15e57579d9a32d6a185fbcc388d
arturisav <artur.isaverdyan@gmail.com>
2018-01-10 13:17:51 +0400

    convert functions to ES6 Arrow

--- 05.Weather/app/components/WeatherMessage.jsx
+++ 05.Weather/app/components/WeatherMessage.jsx
@@ -1,15 +1,26 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-    render: function () {
-        var {loc, temp} = this.props;
+// var WeatherMessage = React.createClass({
+//     render: function () {
+//         var {loc, temp} = this.props;
+//
+//         return (
+//             <div>
+//                 <h3>It is {temp}°C in {loc}</h3>
+//             </div>
+//         );
+//     }
+// });
 
-        return (
-            <div>
-                <h3>It is {temp}°C in {loc}</h3>
-            </div>
-        );
-    }
-});
+var WeatherMessage = ({loc, temp}) => {
+
+    // var {loc, temp} = props;
+
+    return (
+        <div>
+            <h3>It is {temp}°C in {loc}</h3>
+        </div>
+    );
+}
 
 module.exports = WeatherMessage;
