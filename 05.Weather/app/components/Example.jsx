b66ed927141a15162ed5265bef4c76df0cd737f5
arturisav <artur.isaverdyan@gmail.com>
2018-01-11 18:29:23 +0400

    example component

--- 05.Weather/app/components/Example.jsx
+++ 05.Weather/app/components/Example.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Example = React.createClass({
 //     render: function () {
@@ -8,6 +9,24 @@ var React = require('react');
 //     }
 // });
 
-var Example = (props) => (<h3>Example Component</h3>);
+var Example = (props) => {
+    return (
+        <div>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are some example locations to try out </p>
+            <ol>
+                <li>
+                    <Link to={'/?location=Yerevan'}>Yerevan</Link>
+                </li>
+                <li>
+                    <Link to={'/?location=London'}>London</Link>
+                </li>
+                <li>
+                    <Link to={'/?location=Goris'}>Goris</Link>
+                </li>
+            </ol>
+        </div>
+    );
+}
 
 module.exports = Example;
