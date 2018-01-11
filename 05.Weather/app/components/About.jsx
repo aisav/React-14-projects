f8ad93591877b94b5d27ecd44228bd0853a8232a
arturisav <artur.isaverdyan@gmail.com>
2018-01-11 18:41:26 +0400

    About component

--- 05.Weather/app/components/About.jsx
+++ 05.Weather/app/components/About.jsx
@@ -8,6 +8,14 @@ var React = require('react');
 //     }
 // });
 
-var About =  (props) => (<h3>About Component</h3>);
+var About = (props) => {
+    return (
+        <div>
+            <h3 className="text-center">About</h3>
+            <p>This is a weather application, build on React.
+                Created by Artur Isaverdyan</p>
+        </div>
+    );
+}
 
 module.exports = About;
