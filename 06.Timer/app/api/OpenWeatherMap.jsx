ad82338ca9a2a855c36505f8000d04acf6374691
Artur Isaverdyan <artur.isaverdyan@gmail.com>
2018-01-14 07:42:35 -0800

    new timer project

---
+++ 06.Timer/app/api/OpenWeatherMap.jsx
@@ -0,0 +1,24 @@
+var axios = require('axios');
+
+const WEATHER_MAP_URL  =
+    'http://api.openweathermap.org/data/2.5/weather?appid=1d55319993e2676cae5afdf5410cdab8&units=Metric';
+
+module.exports = {
+    getTemp: function (loc) {
+        var encodedLocation = encodeURIComponent(loc);
+        var requestUrl = `${WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+        return axios.get(requestUrl).then(
+            function (res) {
+            // debugger;
+            if (res.data.cod && res.data.message) {
+                throw new Error(res.data.message);
+            } else {
+                return res.data.main.temp;
+            }
+        },
+            function (res) {
+            throw new Error(res.data.message);
+        });
+    }
+}
---
+++ 06.Timer/app/components/Nav.jsx
@@ -0,0 +1,42 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+    onSearch: function (e) {
+        e.preventDefault();
+        var location = this.refs.location.value;
+        var encodedLocation = encodeURIComponent(location);
+        if(location){
+            this.refs.location.value = '';
+            window.location.hash = '#/?location='+encodedLocation;
+        }},
+    render: function () {
+        return (
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">Timer with React</li>
+                        <li>
+                            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/coundown" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <div>
+                        <ul className="menu">
+                            <li className="menu-text">
+                                Created by Artur Isaverdyan
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+});
+
+module.exports = Nav;
