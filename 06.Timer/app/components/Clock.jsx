109799a11651b42e88ba4bdb7d31458e60ca55b3
Artur Isaverdyan <artur.isaverdyan@gmail.com>
2018-01-16 12:28:04 -0800

    clock

--- 06.Timer/app/components/Clock.jsx
+++ 06.Timer/app/components/Clock.jsx
@@ -1,23 +1,30 @@
 var React = require('react');
 
 var Clock = React.createClass({
-    formatSeconds: function (seconds) {
-        var m = Math.floor(seconds/60);
-        var s = seconds%60;
+    getDefaultProps: function () {
+        totalSeconds: 0
+    },
+    propTypes: {
+        totalSeconds: React.PropTypes.number
+    },
+    formatSeconds: function (totalSeconds) {
+        debugger;
+        var m = Math.floor(totalSeconds / 60);
+        var s = totalSeconds % 60;
 
-        m = (m<10) ? ('0' + m) : m;
-        s = (s<10) ? ('0' + s) : s;
+        m = (m < 10) ? ('0' + m) : m;
+        s = (s < 10) ? ('0' + s) : s;
         return m + ':' + s;
     },
 
     render: function () {
-        var totalSeconds = this.props.seconds;
+        var {totalSeconds} = this.props;
 
         return (
             <div className="clock">
-        <span className="clock-text">
-          {this.formatSeconds(totalSeconds)}
-        </span>
+                <span className="clock-text">
+                  {this.formatSeconds(totalSeconds)}
+                </span>
             </div>
         );
 
