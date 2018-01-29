d631685c4bfb8e1f76a1789832fd9d56a943f2df
Artur Isaverdyan <artur.isaverdyan@gmail.com>
2018-01-29 05:22:55 -0800

    redux dispatching

--- 08.LearningRedux/app/redux-example.jsx
+++ 08.LearningRedux/app/redux-example.jsx
@@ -10,10 +10,40 @@ var stateDefault = {
 
 var reducer = (state = stateDefault, action) => {
     // state = state || {name: 'Anonymous'};
-    return state;
+    console.log('New Action', action);
+    switch (action.type) {
+        case 'CHANGE_NAME':
+            return {
+                ...state,
+                name: action.name
+        }
+        case 'CHANGE-SEARCHTEXT':
+            return {
+                ...state,
+                searchText:  action.searchText
+            }
+        default:
+            return state;
+    }
 };
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 
 console.log('currentState', currentState);
+
+var action = {
+    type: 'CHANGE_NAME',      //action name
+    name: 'Artur'
+}
+
+store.dispatch(action);
+
+console.log('Name should be Artur', store.getState().name);
+
+store.dispatch({
+    type: 'CHANGE-SEARCHTEXT',
+    searchText: 'abcd'
+});
+
+console.log('searchText should be abcd', store.getState().searchText);
