e1c21e57dd23a47149c351af4d083ff061f78b9e
Artur Isaverdyan <artur.isaverdyan@gmail.com>
2018-01-30 00:40:27 -0800

    listening changes

--- 08.LearningRedux/app/redux-example.jsx
+++ 08.LearningRedux/app/redux-example.jsx
@@ -3,47 +3,48 @@ var redux = require('redux');
 console.log("22Starting Redux");
 
 var stateDefault = {
-    searchText: '',
+    name: 'Anonymous',
     showCompleted: false,
     todos: []
 }
 
 var reducer = (state = stateDefault, action) => {
     // state = state || {name: 'Anonymous'};
-    console.log('New Action', action);
+    console.log('=======Calling Action===========', action);
     switch (action.type) {
         case 'CHANGE_NAME':
             return {
                 ...state,
                 name: action.name
-        }
-        case 'CHANGE-SEARCHTEXT':
-            return {
-                ...state,
-                searchText:  action.searchText
-            }
+        };
+
         default:
             return state;
     }
 };
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Listening  changes
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+
+  document.getElementById('app').innerHTML = state.name;
+});
+// unsubscribe();
 
 var currentState = store.getState();
-
 console.log('currentState', currentState);
 
-var action = {
+store.dispatch({
     type: 'CHANGE_NAME',      //action name
     name: 'Artur'
-}
-
-store.dispatch(action);
-
-console.log('Name should be Artur', store.getState().name);
-
-store.dispatch({
-    type: 'CHANGE-SEARCHTEXT',
-    searchText: 'abcd'
 });
 
-console.log('searchText should be abcd', store.getState().searchText);
+store.dispatch({
+    type: 'CHANGE_NAME',      //action name
+    name: 'Tom'
+});
+
+
