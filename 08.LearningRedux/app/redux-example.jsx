0468e1f3c06595067f272928aebb26a3d1a1a1ec
Artur Isaverdyan <artur.isaverdyan@gmail.com>
2018-01-30 02:12:03 -0800

    add to arrays actions

--- 08.LearningRedux/app/redux-example.jsx
+++ 08.LearningRedux/app/redux-example.jsx
@@ -4,10 +4,12 @@ console.log("22Starting Redux");
 
 var stateDefault = {
     name: 'Anonymous',
-    showCompleted: false,
-    todos: []
+    hobbies: [],
+    movies: []
 }
 
+var nextHobbyId = 1;
+var nextMovieId = 1;
 var reducer = (state = stateDefault, action) => {
     // state = state || {name: 'Anonymous'};
     console.log('=======Calling Action===========', action);
@@ -16,21 +18,50 @@ var reducer = (state = stateDefault, action) => {
             return {
                 ...state,
                 name: action.name
-        };
+            };
+
+        case 'ADD_HOBBY':
+            return {
+                ...state,
+                hobbies: [
+                    ...state.hobbies,
+                    {
+                        id: nextHobbyId++,
+                        hobby: action.hobby
+                    }
+                ]
+
+            };
+
+        case 'ADD_MOVIE':
+            return {
+                ...state,
+                movies: [
+                    ...state.movies,
+                    {
+                        id: nextMovieId++,
+                        title: action.title,
+                        janre: action.janre
+                    }
+                ]
+            };
 
         default:
             return state;
     }
 };
 var store = redux.createStore(reducer, redux.compose(
-  window.devToolsExtension ? window.devToolsExtension() : f => f
+    window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
 // Listening  changes
 var unsubscribe = store.subscribe(() => {
-  var state = store.getState();
+    var state = store.getState();
 
-  document.getElementById('app').innerHTML = state.name;
+    document.getElementById('app').innerHTML = state.name;
+
+    console.log('====================================handle change', state);
+
 });
 // unsubscribe();
 
@@ -43,8 +74,19 @@ store.dispatch({
 });
 
 store.dispatch({
+    type: 'ADD_HOBBY',      //action name
+    hobby: 'running'
+});
+
+store.dispatch({
     type: 'CHANGE_NAME',      //action name
     name: 'Tom'
+});
+
+store.dispatch({
+    type: 'ADD_MOVIE',      //action name
+    title: 'the title',
+    janre: 'the janre'
 });
 
 
